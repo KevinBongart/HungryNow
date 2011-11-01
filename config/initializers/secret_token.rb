6a76824bdd788ab7d8a9dc849e91b3004fc8adf0
Kevin <contact@kevinbongart.net>
2011-10-31 22:47:42 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryNow::Application.config.secret_token = '80bddb275ecb09d30c581c766ec9bda4218c0786c9950e270f22748c934c0df4773290c1fd0e47c0adcd57bffa7dc5f6dc99314f33bf5412815a70f73a730b7b'
