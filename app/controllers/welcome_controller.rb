1c34f2af02fe3b6e4d02a21f2afe3d38d7c34b52
Kevin <contact@kevinbongart.net>
2011-11-01 22:55:05 -0400

    Home page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,17 @@ class WelcomeController < ApplicationController
   # GET /searches
   # GET /searches.json
   def index
+    values = [
+      'Nutella',
+      'Strawberries',
+      'Chicken, parmesan',
+      'Rice',
+      'Salmon, tomatoes',
+      'Noodles',
+      'Beef, onions',
+      'Tomatoes, mozarella']
+
     @search = Search.new
+    @value  = values[rand(values.length)]
   end
 end
